acd9796fc69b37e63c2654a91b466a1e1d9b2288
Tanveer Singh <tanveerbadhan@gmail.com>
2024-11-08 12:16:49 -0500

    First Commit

---
+++ app/src/main/java/com/example/pizza_tanveer/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pizza_tanveer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pizza_tanveer.databinding.ActivityMainBinding
+
+
+class MainActivity: AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
